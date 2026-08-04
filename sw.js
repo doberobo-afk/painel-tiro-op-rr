c92d812a4d42d3142e6fbeabdc0b4927cdb42c6c
doberobo-afk <doberobo@gmail.com>
2026-08-04 16:23:00 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/android-chrome-192x192.png',
+  '/android-chrome-512x512.png'
 ];
 
 self.addEventListener('install', event => {
